a5c8634bb8173bcc7f32bbd310b021079130360f
Taylor Buckner <tbuckner@theappraisallane.com>
2019-03-20 19:58:56 -0500

    WIP.

--- oop.py
+++ oop.py
@@ -1,24 +1,31 @@
 class Employee:
 
-  raise_amount = 1.04
+  number_of_employees = 0
+  raise_amount = .04
+
   def __init__(self, first, last, pay):
     self.first = first
     self.last = last
     self.pay = pay
     self.email = first + '.' + last + '@company.com'
 
+    Employee.number_of_employees += 1 # Using Employee. instead of self. to overwrite for all instances
+
   def full_name(self):
     return '{} {}'.format(self.first, self.last)
 
   def apply_raise(self):
-    self.pay = int(self.pay * self.raise_amount)
+    self.pay = int(self.pay * self._get_raise_amount())
+
+  def _get_raise_amount(self):
+    return self.raise_amount + 1
 
 emp_1 = Employee('Taylor', 'Buckner', 69000)
 emp_2 = Employee('Anna', 'Buckner', 69000)
 
 
 print(emp_1.full_name())
-print(Employee.full_name(emp_1))
+print(Employee.full_name(emp_1)) # Can pass instance to Class method.
 print(emp_2.full_name())
 
 print(emp_1.pay)
@@ -26,3 +33,4 @@ emp_1.apply_raise()
 print(emp_1.pay)
 
 print(emp_1.__dict__)
+print(emp_1.number_of_employees)
